fe2c227123e6d8d628951fe0dfa7b63d11e99f0d
Henrique Coelho <henriquebc90@gmail.com>
2018-04-17 17:34:34 -0300

    Web API with Register, Delete, Update, Find All and Find By Id.

---
+++ Projeto.DAL/ClienteRepositorio.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entidades;
+using System.Data.SqlClient;
+
+namespace Projeto.DAL
+{
+    public class ClienteRepositorio : Conexao
+    {
+
+        public void Insert(Cliente c)
+        {
+            OpenConnection();
+
+            string query = "insert into Cliente(Nome, Email, DataCadastro) " + "values(@Nome, @Email, @DataCadastro)";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Nome", c.Nome);
+            cmd.Parameters.AddWithValue("@Email", c.Email);
+            cmd.Parameters.AddWithValue("@DataCadastro", c.DataCadastro);
+            cmd.ExecuteNonQuery();
+
+            CloseConnection();
+        }
+
+        public void Update(Cliente c)
+        {
+            OpenConnection();
+
+            string query = "update Cliente set Nome = @Nome, Email = @Email " + "where IdCliente = @IdCliente";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Nome", c.Nome);
+            cmd.Parameters.AddWithValue("@Email", c.Email);
+            cmd.Parameters.AddWithValue("@IdCliente", c.IdCliente);
+            cmd.ExecuteNonQuery();
+
+
+            CloseConnection();
+        }
+
+        public void Delete(int idCliente)
+        {
+            OpenConnection();
+
+            string query = "delete from Cliente where IdCliente = @IdCliente";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@IdCliente", idCliente);
+            cmd.ExecuteNonQuery();
+            
+            CloseConnection();
+        }
+
+        public List<Cliente> FindAll()
+        {
+            OpenConnection();
+
+            string query = "select * from Cliente";
+
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+
+            List<Cliente> lista = new List<Cliente>();
+
+            while (dr.Read())
+            {
+                Cliente c = new Cliente();
+                c.IdCliente = Convert.ToInt32(dr["IdCliente"]);
+                c.Nome = Convert.ToString(dr["Nome"]);
+                c.Email = Convert.ToString(dr["Email"]);
+                c.DataCadastro = Convert.ToDateTime(dr["DataCadastro"]);
+
+                lista.Add(c);
+            }
+            CloseConnection();
+            return lista;
+        }
+
+        public Cliente FindById(int idCliente)
+        {
+            OpenConnection();
+
+            string query = "select * from Cliente where IdCliente = @IdCliente";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@IdCliente", idCliente);
+            dr = cmd.ExecuteReader();
+
+            Cliente c = null;
+
+            if (dr.Read())
+            {
+                c = new Cliente();
+
+                c.IdCliente = Convert.ToInt32(dr["IdCliente"]);
+                c.Nome = Convert.ToString(dr["Nome"]);
+                c.Email = Convert.ToString(dr["Email"]);
+                c.DataCadastro = Convert.ToDateTime(dr["DataCadastro"]);
+
+            }
+
+            CloseConnection();
+            return c;
+
+        }
+
+
+    }
+}
---
+++ Projeto.Services/Controllers/ClienteController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Projeto.DAL;
+using Projeto.Entidades;
+using Projeto.Services.Models;
+
+namespace Projeto.Services.Controllers
+{
+
+    [RoutePrefix("api/cliente")]
+    public class ClienteController : ApiController
+    {
+        [HttpPost]
+        [Route("cadastrar")]
+        public HttpResponseMessage Cadastrar(ClienteCadastroViewModel model)
+        {
+            try
+            {
+                Cliente c = new Cliente();
+                c.Nome = model.Nome;
+                c.Email = model.Email;
+                c.DataCadastro = DateTime.Now;
+
+                ClienteRepositorio rep = new ClienteRepositorio();
+                rep.Insert(c);
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Cliente cadastrado com sucesso");
+
+
+            }
+            catch (Exception e)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("atualizar")]
+        public HttpResponseMessage Atualizar(ClienteEdicaoViewModel model)
+        {
+
+            try
+            {
+                Cliente c = new Cliente();
+                c.IdCliente = model.IdCliente;
+                c.Nome = model.Nome;
+                c.Email = model.Email;
+
+                ClienteRepositorio rep = new ClienteRepositorio();
+                rep.Update(c);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Cliente atualizado com sucesso");
+
+            }
+            catch (Exception e)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+            
+        }
+
+        [HttpDelete]
+        [Route("excluir")]
+        public HttpResponseMessage Excluir (int id)
+        {
+            try
+            {
+
+                ClienteRepositorio rep = new ClienteRepositorio();
+                rep.Delete(id);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Cliente excluido com sucesso");
+                
+
+            }
+            catch (Exception e)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("consultar")]
+        public HttpResponseMessage Consultar()
+        {
+
+            try
+            {
+
+                List<ClienteConsultaViewModel> lista = new List<ClienteConsultaViewModel>();
+
+                ClienteRepositorio rep = new ClienteRepositorio();
+
+                foreach (Cliente c in rep.FindAll())
+                {
+                    ClienteConsultaViewModel model = new ClienteConsultaViewModel();
+
+                    model.IdCliente = c.IdCliente;
+                    model.Nome = c.Nome;
+                    model.Email = c.Email;
+                    model.DataCadastro = c.DataCadastro;
+
+                    lista.Add(model);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, lista);
+
+            }
+            catch (Exception e)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+            
+        }
+
+
+        [HttpGet]
+        [Route("obterporid")]
+        public HttpResponseMessage ObterPorId (int id)
+        {
+
+            try
+            {
+
+                ClienteRepositorio rep = new ClienteRepositorio();
+                Cliente c = rep.FindById(id);
+
+                if(c != null)
+                {
+                    ClienteConsultaViewModel model = new ClienteConsultaViewModel();
+
+                    model.IdCliente = c.IdCliente;
+                    model.Nome = c.Nome;
+                    model.Email = c.Email;
+                    model.DataCadastro = c.DataCadastro;
+
+
+                    return Request.CreateResponse(HttpStatusCode.OK, model);
+
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Cliente não foi encontrado");
+                }
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message); ;
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
